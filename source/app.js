8024d2ff25dac358fa06a9befc25fedc56298add
Rich Caloggero <rjc@mit.edu>
2018-05-04 19:18:36 -0400

    * added box-shadow

---
+++ source/app.js
@@ -0,0 +1,14 @@
+let $ = require ("./enhanceTable.jquery.js");
+let util = require ("./util.js");
+let fields = ["musician", "instrument", "genre"];
+let data = [
+["Jerry Garcia", "guitar", "rock"],
+["Elton John", "piano", "rock"],
+["Elvin Jones", "drums", "jazz"],
+["Yoyo Mah", "cello", "classical"],
+["Rich Caloggero", "guitar", "jam"]
+];
+
+
+util.createTable ("#musicians", fields, data);
+$("table").enhanceTable();
